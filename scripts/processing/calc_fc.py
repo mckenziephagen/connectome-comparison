d2930e022538bca88a698bfb0dbbbb54f9f7cebd
mckenziephagen <mphagen@uw.edu>
2025-03-24 17:50:55 -0700

    update for corresponding changes in model package

--- scripts/processing/calc_fc.py
+++ scripts/processing/calc_fc.py
@@ -21,7 +21,7 @@ from numpy.matlib import repmat
 # -
 
 #work around until I install fc_comparison as an actual package
-sys.path.append(os.path.dirname('/global/homes/m/mphagen/functional_connectivity_comparison/fc_comparison'))
+sys.path.append(os.path.dirname('/global/homes/m/mphagen/functional-connectivity/model-fc/src/model_fc'))
 
 # +
 import time
@@ -47,7 +47,7 @@ from sklearn.preprocessing import StandardScaler
 
 import pickle
 
-from fc_comparison.models import init_model, run_model
+from model_fc.models import init_model, run_model
 
 # +
 args = argparse.Namespace()
@@ -55,13 +55,12 @@ args = argparse.Namespace()
 parser = argparse.ArgumentParser()
 parser.add_argument('--subject_id',default='112516')
 parser.add_argument('--ses_id', default='ses-1') 
-parser.add_argument('--combine_runs', default=True) 
 
 parser.add_argument('--atlas_name', default='schaefer')
 parser.add_argument('--n_rois', default=100, type=int) #default for hcp; 
 parser.add_argument('--n_trs', default=1200, type=int) #default for hcp;
 parser.add_argument('--n_folds', default=5) 
-parser.add_argument('--model', default='correlation') 
+parser.add_argument('--model', default='lasso-bic') 
 parser.add_argument('--cv', default='blocks') 
 
 parser.add_argument('--fc_data_path', 
@@ -74,10 +73,11 @@ try:
     args = parser.parse_args()
 except KeyError: 
     args = parser.parse_args([])
+    notebook = True
+    results_path = 'notebook_results'
     
 subject_id = args.subject_id
 ses_id = args.ses_id
-combine_runs = args.combine_runs
 atlas_name = args.atlas_name
 n_rois = args.n_rois
 n_trs = args.n_trs
@@ -86,7 +86,7 @@ model_str = args.model
 cv = args.cv
 fc_data_path = args.fc_data_path
 
-max_iter = 1000
+max_iter = args.max_iter
 
 random_state = 1
 print(args)
@@ -100,12 +100,12 @@ ts_files = glob(op.join(fc_data_path,
                         f'{ses_id}*',
                         '*',
                         '*.tsv'))
-
-results_path = op.join(fc_data_path, 
-                       'derivatives',
-                       'connectivity-matrices',
-                       f'{model_str}-{cv}',
-                       f'sub-{subject_id}')
+if notebook == False:
+    results_path = op.join(fc_data_path, 
+                    'derivatives',
+                    'connectivity-matrices',
+                    f'{model_str}-{cv}',
+                    f'sub-{subject_id}')
                        
 os.makedirs(results_path, exist_ok=True)
 
@@ -160,21 +160,19 @@ time_series = np.append(read_ts(ts_files[0], n_trs, n_rois),
 
 
 # +
+results_dict = {}    
+file = f'sub-{subject_id}_{atlas_name}-{n_rois}_task-Rest_{ses_id}_fc-{model_str}.pkl'
+print(f"Calculating {model_str} FC for {ses_id}")
+
+scaler = StandardScaler()
+
 if model_str in ["lasso-cv", "uoi-lasso", "enet", "lasso-bic"] :
-    print(f"Calculating {model_str} FC for {ses_id}")
-    
-    
     splits = split_kfold(cv, time_series)
-
-    fc_mats = {}
-    rsq_dict = {}
-
     for fold_idx, (train_idx, test_idx) in enumerate( splits ): 
         
         print(f"Fold {fold_idx}:")
         print(f"  Train: range={train_idx[0]}, {train_idx[-1]}")
         print(f"  Test:  index={test_idx[0]}, {test_idx[-1]}")
-
 
         train_ts = time_series[train_idx, :]
         test_ts = time_series[test_idx, :]
@@ -184,39 +182,17 @@ if model_str in ["lasso-cv", "uoi-lasso", "enet", "lasso-bic"] :
         scaler.transform(test_ts)
 
         start_time = time.time()
-        fc_mats[fold_idx], rsq_dict[fold_idx], tst_model = run_model(train_ts, 
-                                                                   test_ts, 
-                                                                   n_rois, 
-                                                                   model=model)
+        results_dict[f'fold_{fold_idx}'] = run_model(train_ts, test_ts, n_rois, model=model)
 
-        print(time.time() - start_time, ' seconds') 
-        print(np.mean(rsq_dict[fold_idx]['test'])) 
+        print(time.time() - start_time, ' seconds')     
 
-    #TODO: helper function to manage filenames 
-    mat_file = f'sub-{subject_id}_{atlas_name}-{n_rois}_task-Rest_{ses_id}_fc-{model_str}_model.pkl'
-   
-    with open(op.join(results_path,mat_file), 'wb') as f:
-        pickle.dump(fc_mats, f)
-    
-    with open(op.join(results_path, 
-                      f'sub-{subject_id}_{atlas_name}-{n_rois}_task-Rest_{ses_id}_fc-{model_str}_r2.pkl'), 
-              'wb') as f: 
-        pickle.dump(rsq_dict, f)
-        
 elif model_str in ['correlation', 'tangent']: 
-    print(f"Calculating {model_str} FC for {ses_id}")
 
     corr_mat = model.fit_transform([time_series])[0]
-    np.fill_diagonal(corr_mat, 0)
+    np.fill_diagonal(corr_mat, 1)
 
-    mat_file = f'sub-{subject_id}_{atlas_name}-{n_rois}_task-Rest_{ses_id}_fc-{model_str}_desc-kfold{cv}.pkl'
-
-    with open(op.join(results_path,mat_file), 'wb') as f:
-        pickle.dump(corr_mat, f) 
-# -
-
-import matplotlib.pyplot as plt
-
-plt.hist(np.arctanh(corr_mat.ravel()) ) 
-
-
+    results_dict['fc_matrix'] = corr_mat
+    results_dict['model'] = model
+    
+with open(op.join(results_path,file), 'wb') as f:
+    pickle.dump(results_dict, f) 
