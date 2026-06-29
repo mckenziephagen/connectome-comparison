7bd3e951d869bc9e626b86d98905a476c845a698
Griffyn Skye Kolpin <gkolpin@n3096.hyak.local>
2026-06-29 10:35:21 -0700

    refactor calc fc

--- pytest/test_fc.py
+++ pytest/test_fc.py
@@ -1,11 +1,67 @@
 import pytest
+import subprocess
+import sys
+import os
+import pandas as pd
+import nitime
+import numpy as np
 
-def test_arbitrary(): 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+print(script_dir)
+
+from model_fc import models
+sys.path.insert(0, os.path.abspath('../scripts/processing'))
+from calc_fc import split_kfold, fit_model
+
+def test_arbitrary():
     assert 1 == 1
+
+def test_fc_runs():
+    script_path = os.path.abspath(os.path.join(script_dir, "..", "scripts", "processing", 'calc_fc.py'))
+    result = subprocess.run([sys.executable, script_path], capture_output=True, text=True)
     
+    assert result.returncode == 0, f"Script crashed with error:\n{result.stderr}"
+
+def test_fc_split():
+
+    data_path = os.path.join(nitime.__path__[0], 'data')
+    fname = os.path.join(data_path, 'fmri_timeseries.csv')
+    dataframe = pd.read_csv(fname, dtype=float, delimiter=',').T
+    data = np.array(dataframe)
+    assert not dataframe.empty
+
+    splits_len = 6
+    for cv_kind in ['random', 'blocks']:
+        splits, timeseries = split_kfold(cv_kind, data, splits_len)
+        for fold_idx, (train_idx, test_idx) in enumerate(splits):
+            assert fold_idx <= splits_len
+            train_ts = timeseries[train_idx, :]
+            test_ts = timeseries[test_idx, :]
+            assert not train_ts.size == 0
+            assert not test_ts.size == 0
+
+def test_fc_on_nitimedata():
+    
+    data_path = os.path.join(nitime.__path__[0], 'data')
+    fname = os.path.join(data_path, 'fmri_timeseries.csv')
+    dataframe = pd.read_csv(fname, dtype=float, delimiter=',').T
+    data = np.array(dataframe)
+    assert not dataframe.empty
+    
+    n_rois = len(data[0])
+    cv_kind = 'random'
+    splits_len = 6
+    splits, timeseries = split_kfold(cv_kind, data, splits_len)
+
+    results_list=[]
+    for model_str in ['correlation', 'lassoCV', 'uoiLasso']:
+        results_dict = fit_model(model_str, splits, timeseries, 1000, n_rois, 10)
+        assert results_dict
+        results_list.append(results_dict)
+    #TO DO: test that they are different
+
 def test_fc_consistency(): 
     """Test whether calc_fc is outputting a consistent matrix. 
-    
     """
     
     #read in exemplar matrix
--- scripts/processing/calc_fc.py
+++ scripts/processing/calc_fc.py
@@ -8,7 +8,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.17.3
 #   kernelspec:
-#     display_name: Python (FC)
+#     display_name: FC
 #     language: python
 #     name: fc
 # ---
@@ -18,7 +18,11 @@
 #
 #
 
+<<<<<<< HEAD
 # %%
+=======
+# +
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
 import sys
 import os
 import numpy.matlib
@@ -26,39 +30,43 @@ from numpy.matlib import repmat
 import nibabel as nib
 import json
 from sklearn.exceptions import ConvergenceWarning
+import sklearn
 import warnings
+<<<<<<< HEAD
 from copy import deepcopy
 import argparse
 
 sys.path.append(os.path.dirname('/mmfs1/gscratch/escience/mphagen/model-fc/src/model_fc'))
 
 # %%
+=======
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
 import time
-
 import nilearn
-
 from pyuoi.utils import log_likelihood_glm, AIC, BIC
-
 import numpy as np
-
 import argparse
-
 import os
 import os.path as op
-
 from glob import glob
-
 from sklearn.model_selection import KFold, GroupKFold, TimeSeriesSplit
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
+<<<<<<< HEAD
 from sklearn.base import clone
 import sklearn 
 
+=======
+from sklearn.linear_model import lars_path
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
 import pickle
 
 from model_fc.models import init_model, run_model
+
+
 # -
 
+<<<<<<< HEAD
 help(init_model) 
 
 # %%
@@ -126,6 +134,8 @@ print(args)
 
 args_dict = vars(args) 
 
+=======
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
 
 # %%
 args_dict = vars(args) 
@@ -155,11 +165,12 @@ def get_ts_files(fc_data_path, proc_type, sub_id, task_id, ses_id, run_id):
                 'timeseries',
                 proc_type,
                 f'sub-{sub_id}',
-                f'sub-{sub_id}*task-{task_id}*ptseries.nii'))
+                f'sub-{sub_id}*task-{task_id}_*ptseries.nii')) #added the singleband here so it should run for participants with multiple scans
 
         return ts_file
 
 
+<<<<<<< HEAD
 # %%
 ts_files = get_ts_files(fc_data_path, proc_type, sub_id, task_id, ses_id, run_id)
 
@@ -193,18 +204,45 @@ print(model)
 
 
 # %%
+=======
+def filter_ts_files(ts_files):
+    #if multiple rest scans, selecting out the singeband one, if none are singleband, just select the first one
+    if len(ts_files) > 1:
+        singlebands = [file for file in ts_files if 'singleband' in file]
+        if singlebands:
+            ts_files = [singlebands[0]]
+        else:
+            ts_files = [ts_files[0]]
+
+    assert len(ts_files) == 1 
+    return ts_files
+
+
+def read_cifti(file): 
+    time_series = nib.load(file).get_fdata(dtype=np.float32)
+    if n_rois != time_series.shape[1]: 
+        time_series = time_series[:, :n_rois]
+    return time_series
+
+
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
 def set_warnings_filters():
     warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
-set_warnings_filters()
 
+<<<<<<< HEAD
 
 # %%
 def split_kfold(cv, time_series, n_folds): 
+=======
+def split_kfold(cv, time_series, n_folds, seed=None):
+    if seed is not None: 
+        pass
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
     if cv == 'random': 
         
         kfolds = KFold(n_splits=n_folds,
-                      shuffle=True)
+                      shuffle=False)
         
         splits = kfolds.split(X=time_series)
         
@@ -223,6 +261,7 @@ def split_kfold(cv, time_series, n_folds):
     if cv == 'timeseries': 
         tscv = TimeSeriesSplit()
         splits = tscv(tscv.split(X=time_series))
+<<<<<<< HEAD
 
     if cv == 'ses': 
         ses_len = int(len(time_series)/2)
@@ -241,12 +280,16 @@ def split_kfold(cv, time_series, n_folds):
         run = sklearn.model_selection.PredefinedSplit(run_idx)
         splits = run.split(time_series)
 
+=======
+    
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
     if cv == 'task': 
         task_file = glob(op.join(fc_data_path, 
                         'derivatives', 
                         'timeseries',
                         'xcpd',
                         f'sub-{sub_id}',
+<<<<<<< HEAD
                         f'sub-{sub_id}*task-{cv_task}_space*ptseries.nii')) 
         task_scan =  nib.load(task_file).get_fdata(dtype=np.float32)
         time_series = time_series.append(task_scan)
@@ -358,6 +401,160 @@ with open(op.join(results_path,file), 'wb') as f:
 config_file = file.replace('_results.pkl', '_results.json')
 with open(op.join(results_path, config_file), "w") as f:
     json.dump(args_dict, f, indent=4)
+=======
+                        f'sub-{sub_id}*task-{cv_task}_*space*ptseries.nii'))[0]
+        task_scan =  nib.load(task_file).get_fdata(dtype=np.float32)[:, :n_rois]
+        time_series = np.append(time_series, task_scan, axis=0)
+
+        ses_len = int(len(time_series)/2)
+        ses_idx = np.concat([repmat(0, 1, ses_len),
+                             repmat(1, 1, ses_len)],axis=-1).ravel()
+        ses = sklearn.model_selection.PredefinedSplit(ses_idx)
+        splits = ses.split(time_series)
+        
+    return splits, time_series
 
 
+def fit_model(model_str, splits, time_series, max_iter, n_rois, random_state, sub_id='Test', ses_id='Test'):
+    results_dict = {}    
+    print(f"Calculating {model_str} FC for {sub_id} {ses_id}")
+    
+    scaler = StandardScaler()
+    model = init_model(model_str, max_iter, random_state)
+    print(model)
+    
+    if model_str in ["lassoCV", "uoiLasso", "enet", "lassoBIC", "ridgeCV"] :
+        print(model_str) 
+    
+        for fold_idx, (train_idx, test_idx) in enumerate(splits): 
+            
+            print(f"Fold {fold_idx}:")
+            print(f"  Train: range={train_idx[0]}, {train_idx[-1]}")
+            print(f"  Test:  index={test_idx[0]}, {test_idx[-1]}")
+    
+            train_ts = time_series[train_idx, :]
+            test_ts = time_series[test_idx, :]
+            
+            scaler = StandardScaler()
+            train_ts = scaler.fit_transform(train_ts)
+            test_ts = scaler.transform(test_ts)
+    
+            start_time = time.time()
+            results_dict[f'fold_{fold_idx}'] = run_model(train_ts, test_ts,
+                                                         n_rois, model=model)
+            results_dict[f'fold_{fold_idx}']['model'] = model
+            print(time.time() - start_time, ' seconds')    
+            print(model)
+                
+            return results_dict 
+        
+    elif model_str in ['correlation', 'tangent']: 
+        corr_mat = model.fit_transform(np.array(time_series))[0]
+        np.fill_diagonal(corr_mat, 1)
+    
+        results_dict['fc_matrix'] = corr_mat
+        results_dict['model'] = model
 
+    return results_dict
+
+
+if __name__ == "__main__":
+    args = argparse.Namespace()
+>>>>>>> e4fb8a1 (update calc_fc for task cv and uoi and add tests)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fff", help="a dummy argument to fool ipython", default="1")
+    #https://stackoverflow.com/questions/48796169/how-to-fix-ipykernel-launcher-py-error-unrecognized-arguments-in-jupyter
+    
+    parser.add_argument('--sub_id',default='sub-3891810238')
+    parser.add_argument('--ses_id', default=1)
+    parser.add_argument('--run_id', default=None)
+    parser.add_argument('--task_id', default='rest') 
+    
+    parser.add_argument('--atlas_spec', default='fsLR_seg-4S156Parcels_den-91k')
+    parser.add_argument('--n_rois', default=100, type=int) #default for hcp; 
+    parser.add_argument('--n_trs', default=1200, type=int) #default for hcp;
+    parser.add_argument('--n_folds', default=7)
+    #must be larger than 6 for PNC
+    parser.add_argument('--model', default='lassoCV') 
+    parser.add_argument('--cv', default='task')
+    parser.add_argument('--proc_type', default='xcpd') 
+    
+    #TO DO: DEFAULT SHOULD BE BASED ON GSCRATCH OR PSCRATCH
+    parser.add_argument('--fc_data_path', 
+                        default='/gscratch/psych/gkolpin/data/pnc_xcpd_4S156Parcels')
+    parser.add_argument('--max_iter', default=1000) 
+    parser.add_argument('--cv_task', default='idemo')
+
+    #TO DO: config file to get this
+    sys.path.append(os.path.dirname('/mmfs1/gscratch/escience/gkolpin/model-fc/src/model_fc'))
+
+    set_warnings_filters()
+    
+    #hack argparse to be jupyter friendly AND cmdline compatible
+    try: 
+        os.environ['_']
+        args = parser.parse_args()
+        notebook = False
+    except KeyError: 
+        args = parser.parse_args([])
+        notebook = True
+        results_path = '/gscratch/scrubbed/gkolpin/xcpd_output/pnc_xcpd_4S156Parcels/derivatives/connectivity-matrices/xcpd'
+    
+    args.sub_id = str(args.sub_id).replace('sub-', '')
+    sub_id = args.sub_id
+    args.ses_id = str(args.ses_id).replace('ses-', '')
+    ses_id = args.ses_id
+    task_id = args.task_id
+    atlas_spec = args.atlas_spec 
+    n_rois = args.n_rois
+    n_trs = args.n_trs
+    n_folds = args.n_folds
+    model_str = args.model
+    cv = args.cv
+    fc_data_path = args.fc_data_path
+    proc_type = args.proc_type
+    cv_task = args.cv_task
+    
+    max_iter = args.max_iter
+    run_id = args.run_id
+    random_state = 1
+    print(args)
+
+    args_dict = vars(args) 
+
+    #TO DO: HCP file names? will they work with this
+    ts_files = get_ts_files(fc_data_path, proc_type, sub_id, task_id, ses_id, run_id)
+    ts_files = filter_ts_files(ts_files)
+    
+
+    if notebook == False:
+        #TO DO: CHANGED TO GET A TEST RESULT PATH, CHANGE BACK LATER
+        results_path = op.join(fc_data_path, 
+                        'derivatives',
+                        'connectivity-matrices',
+                        'test', 
+                        f"{model_str.replace('-', '')}_{cv}",
+                        f'sub-{sub_id}')
+    
+    os.makedirs(results_path, exist_ok=True)
+    
+    
+    print(f"Found {len(ts_files)} rest scans for subject {sub_id}.") 
+    
+    print(f"Saving results to {results_path}.")
+
+    time_series = read_cifti(ts_files[0])
+    
+    splits, time_series = split_kfold(cv, time_series, n_folds)
+
+    results_dict = fit_model(model_str, splits, time_series, max_iter, n_rois, random_state, sub_id, ses_id)
+    
+    file = f"sub-{sub_id}_task-{task_id}_ses-{ses_id}_{atlas_spec}_model-{model_str.replace('-', '')}_results.pkl"
+        
+    with open(op.join(results_path,file), 'wb') as f:
+        pickle.dump(results_dict, f) 
+    
+    config_file = file.replace('_results.pkl', '_results.json')
+    with open(op.join(results_path, config_file), "w") as f:
+        json.dump(args_dict, f, indent=4)
