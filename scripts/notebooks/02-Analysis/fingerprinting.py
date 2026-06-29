aec5d86bd33d1bcbee9d31126316efc6f5fe5ed7
mckenziephagen <mphagen@uw.edu>
2026-06-29 10:43:24 -0700

    add models and proc types to fingerprinting

--- scripts/notebooks/02-Analysis/fingerprinting.py
+++ scripts/notebooks/02-Analysis/fingerprinting.py
@@ -21,42 +21,82 @@ import pickle
 import random
 from glob import glob
 
+# ### HCP 
+
 # +
 results_path = f'/global/homes/m/mphagen/functional-connectivity/connectome-comparison/results'
+with open(glob(op.join(results_path, 'MSMAll', f'*12-10*lassoBIC*pkl'))[0], 'rb') as l:
+    old_fix_lasso_dict = pickle.load(l)
 
-with open(glob(op.join(results_path,'MSMAll', '*12-10*lassoBIC*pkl'))[0], 'rb') as l:
-    msmall_lasso_dict = pickle.load(l)
+_ = [old_fix_lasso_dict[sub].pop("ses-full") for sub in old_fix_lasso_dict.keys()] 
 
-_ = [msmall_lasso_dict[sub].pop("ses-full") for sub in msmall_lasso_dict.keys()] 
+# +
+with open(glob(op.join(results_path,'xcpd', '*12-10*lassoBIC*pkl'))[0], 'rb') as l:
+    old_xcpd_lasso_dict = pickle.load(l)
 
-with open(glob(op.join(results_path,'MSMAll', '*12-10*corr*pkl'))[0], 'rb') as l:
-    msmall_pearson_dict = pickle.load(l)
+_= [old_xcpd_lasso_dict[sub].pop("ses-full") for sub in old_xcpd_lasso_dict.keys()] 
 
-_= [msmall_pearson_dict[sub].pop("ses-full") for sub in msmall_pearson_dict.keys()] 
 
-with open(glob(op.join(results_path,'MSMAll', '*12-10*uoi*pkl'))[0], 'rb') as l:
-    msmall_uoi_dict = pickle.load(l)
+# +
+date_str = '2026-05-22'
 
-_ = [msmall_uoi_dict[sub].pop("ses-full") for sub in msmall_uoi_dict.keys()] 
+results_path = f'/global/homes/m/mphagen/functional-connectivity/connectome-comparison/results'
+proc_type = 'MSMAll_FIX'
 
-### 
-with open(glob(op.join(results_path,'xcpd', '*12-10*corr*pkl'))[0], 'rb') as l:
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*lassoBIC*pkl'))[0], 'rb') as l:
+    fix_lasso_dict = pickle.load(l)
+
+_ = [fix_lasso_dict[sub].pop("ses-full") for sub in fix_lasso_dict.keys()] 
+
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*ridgeCV*pkl'))[0], 'rb') as l:
+    fix_ridge_dict = pickle.load(l)
+
+_ = [fix_ridge_dict[sub].pop("ses-full") for sub in fix_ridge_dict.keys()] 
+
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*-correlation*pkl'))[0], 'rb') as l:
+    fix_pearson_dict = pickle.load(l)
+
+_= [fix_pearson_dict[sub].pop("ses-full") for sub in fix_pearson_dict.keys()] 
+
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*partial_corr*pkl'))[0], 'rb') as l:
+    fix_pcorr_dict = pickle.load(l)
+
+_= [fix_pcorr_dict[sub].pop("ses-full") for sub in fix_pcorr_dict.keys()] 
+
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*uoi*pkl'))[0], 'rb') as l:
+    fix_uoi_dict = pickle.load(l)
+
+_ = [fix_uoi_dict[sub].pop("ses-full") for sub in fix_uoi_dict.keys()] 
+
+# ###### 
+proc_type = 'xcpd'
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*-corr*pkl'))[0], 'rb') as l:
     xcpd_pearson_dict = pickle.load(l)
 
 _= [xcpd_pearson_dict[sub].pop("ses-full") for sub in xcpd_pearson_dict.keys()] 
 
-with open(glob(op.join(results_path,'xcpd', '*12-10*lassoBIC*pkl'))[0], 'rb') as l:
+with open(glob(op.join(results_path,proc_type, f'{date_str}*lassoBIC*pkl'))[0], 'rb') as l:
     xcpd_lasso_dict = pickle.load(l)
 
 _= [xcpd_lasso_dict[sub].pop("ses-full") for sub in xcpd_lasso_dict.keys()] 
 
+with open(glob(op.join(results_path,proc_type, f'*{date_str}*ridgeCV*pkl'))[0], 'rb') as l:
+    xcpd_ridge_dict = pickle.load(l)
 
+_= [xcpd_ridge_dict[sub].pop("ses-full") for sub in xcpd_ridge_dict.keys()] 
 
-with open(glob(op.join(results_path,'xcpd', '*12-10*uoi*pkl'))[0], 'rb') as l:
+with open(glob(op.join(results_path,'xcpd', f'*{date_str}*uoi*pkl'))[0], 'rb') as l:
     xcpd_uoi_dict = pickle.load(l)
 
 _= [xcpd_uoi_dict[sub].pop("ses-full") for sub in xcpd_uoi_dict.keys()] 
 
+
+# +
+#### PNC 
+# with open(glob(op.join(results_path,'PNC', f'*{date_str}*lassoBIC*pkl'))[0], 'rb') as l:
+#     pnc_lasso_dict = pickle.load(l)
+
+# _ = [pnc_lasso_dict[sub].pop("ses-full") for sub in pnc_lasso_dict.keys()] 
 # -
 
 def fingerprint(target_dict, database_dict): 
@@ -86,53 +126,89 @@ def fingerprint(target_dict, database_dict):
 
 # note for future self: max(corr_dict ...) returns the keys associated with the highest key.value(). 
 
-msmall_dict = {} 
-msmall_dict['lasso'] = {} 
-msmall_dict['pearson'] = {} 
-msmall_dict['uoi'] = {} 
+# +
+fix_dict = {} 
+fix_dict['lasso'] = {} 
+fix_dict['pearson'] = {} 
+fix_dict['uoi'] = {} 
+fix_dict['pcorr'] = {} 
+
+fix_dict['thresh_lasso'] = {} 
+fix_dict['thresh_uoi'] = {} 
+fix_dict['ridge'] = {} 
 
 # +
-corr_dict, fp_list, _ = fingerprint(msmall_lasso_dict, msmall_lasso_dict)
+lasso_dict, fp_list, _ = fingerprint(fix_lasso_dict, fix_lasso_dict)
 
 acc_list = [] 
 
 for i in range(1000): 
-    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(msmall_lasso_dict)) 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
     
 acc_list.sort()
 
-msmall_dict['lasso']['avg'] = np.mean(acc_list)
-msmall_dict['lasso']['lower'] =    acc_list[50]
-msmall_dict['lasso']['upper'] =    acc_list[-50]
-msmall_dict['lasso']['boot_vals'] = acc_list
+fix_dict['lasso']['avg'] = np.mean(acc_list)
+fix_dict['lasso']['lower'] =    acc_list[50]
+fix_dict['lasso']['upper'] =    acc_list[-50]
+fix_dict['lasso']['boot_vals'] = acc_list
 
 
 print("average: ", np.mean(acc_list))
 print("CI: ", acc_list[50], acc_list[-50]) 
-# -
-
-np.array(corr_dict[key].values())
-
-corr_dict[key].get() 
-
-key
-
-corr_dict[key]['sub-979984']
-
-key
-
-max(corr_dict[key]) 
-
-for key in corr_dict.keys():
-    print(key) 
-    # print(key,value)
-    print( key.split('_')[0]) 
-    print(key,  max(corr_dict[key],key=corr_dict[key].get), key in (max(corr_dict[key],key=corr_dict[key].get)))
-
-pd.DataFrame(corr_dict['sub-793465']) 
 
 # +
-_, fp_list, _ = fingerprint(msmall_pearson_dict, msmall_pearson_dict)
+_, fp_list, _ = fingerprint(fix_ridge_dict, fix_ridge_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
+    
+acc_list.sort()
+
+fix_dict['ridge']['avg'] = np.mean(acc_list)
+fix_dict['ridge']['lower'] =    acc_list[50]
+fix_dict['ridge']['upper'] =    acc_list[-50]
+fix_dict['ridge']['boot_vals'] = acc_list
+
+
+print("average: ", np.mean(acc_list))
+print("CI: ", acc_list[50], acc_list[-50]) 
+
+# +
+_, fp_list, _ = fingerprint(fix_pcorr_dict, fix_pcorr_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
+    
+acc_list.sort()
+
+fix_dict['pcorr']['avg'] = np.mean(acc_list)
+fix_dict['pcorr']['lower'] =    acc_list[50]
+fix_dict['pcorr']['upper'] =    acc_list[-50]
+fix_dict['pcorr']['boot_vals'] = acc_list
+
+
+print("average: ", np.mean(acc_list))
+print("CI: ", acc_list[50], acc_list[-50]) 
+
+# +
+old_lasso_dict, fp_list, _ = fingerprint(old_fix_lasso_dict, old_fix_lasso_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
+    
+acc_list.sort()
+
+print("average: ", np.mean(acc_list))
+print("CI: ", acc_list[50], acc_list[-50]) 
+
+# +
+res_fix_pearson, fp_list, _ = fingerprint(fix_pearson_dict, fix_pearson_dict)
 
 acc_list = [] 
 for i in range(1000): 
@@ -140,16 +216,16 @@ for i in range(1000):
 acc_list.sort()
 
 
-msmall_dict['pearson']['avg'] = np.mean(acc_list)
-msmall_dict['pearson']['lower'] =  acc_list[50]
-msmall_dict['pearson']['upper'] =   acc_list[-50]
-msmall_dict['pearson']['boot_vals'] = acc_list
+fix_dict['pearson']['avg'] = np.mean(acc_list)
+fix_dict['pearson']['lower'] =  acc_list[50]
+fix_dict['pearson']['upper'] =   acc_list[-50]
+fix_dict['pearson']['boot_vals'] = acc_list
 
 print("average: ", np.mean(acc_list))
 print("CI: ", acc_list[50], acc_list[-50]) 
 
 # +
-_, fp_list, _ = fingerprint(msmall_uoi_dict, msmall_uoi_dict)
+_, fp_list, _ = fingerprint(fix_uoi_dict, fix_uoi_dict)
 
 acc_list = [] 
 
@@ -157,19 +233,44 @@ for i in range(1000):
     acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
 acc_list.sort()
 
-msmall_dict['uoi']['avg'] = np.mean(acc_list)
-msmall_dict['uoi']['lower'] =    acc_list[50]
-msmall_dict['uoi']['upper'] =    acc_list[-50]
-msmall_dict['uoi']['boot_vals'] = acc_list
+fix_dict['uoi']['avg'] = np.mean(acc_list)
+fix_dict['uoi']['lower'] =    acc_list[50]
+fix_dict['uoi']['upper'] =    acc_list[-50]
+fix_dict['uoi']['boot_vals'] = acc_list
 
 print("average: ", np.mean(acc_list))
 print("CI: ", acc_list[50], acc_list[-50]) 
-# -
 
+# +
+# pnc_dict = {} 
+# pnc_dict['lasso'] = {} 
+# pnc_dict['pearson'] = {} 
+
+# +
+# _, fp_list, _ = fingerprint(pnc_lasso_dict, pnc_lasso_dict)
+
+# acc_list = [] 
+
+# for i in range(1000): 
+#     acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(fp_list)) 
+# acc_list.sort()
+
+# pnc_dict['lasso']['avg'] = np.mean(acc_list)
+# pnc_dict['lasso']['lower'] =    acc_list[50]
+# pnc_dict['lasso']['upper'] =    acc_list[-50]
+# pnc_dict['lasso']['boot_vals'] = acc_list
+
+# print("average: ", np.mean(acc_list))
+# print("CI: ", acc_list[50], acc_list[-50]) 
+
+# +
 xcpd_dict = {} 
 xcpd_dict['lasso'] = {} 
 xcpd_dict['pearson'] = {} 
 xcpd_dict['uoi'] = {} 
+
+xcpd_dict['thresh_lasso'] = {} 
+xcpd_dict['thresh_uoi'] = {} 
 
 # +
 _, fp_list, _ = fingerprint(xcpd_pearson_dict, xcpd_pearson_dict)
@@ -227,19 +328,134 @@ print("average: ", np.mean(acc_list))
 print("CI: ", acc_list[50], acc_list[-50]) 
 # -
 
+fix_boot_df = pd.DataFrame.from_dict([fix_dict['lasso']['boot_vals'], 
+                                         fix_dict['pearson']['boot_vals'],
+                                         fix_dict['uoi']['boot_vals'] , 
+                                        fix_dict['ridge']['boot_vals'] , 
+                                        fix_dict['pcorr']['boot_vals']] ) 
+
+                                         # fix_dict['thresh_lasso']['boot_vals'] , 
+                                         # fix_dict['thresh_uoi']['boot_vals']])
+fix_boot_df.index = ['lasso', 'pearson', 'uoi', 'ridge', 'pcorr'] 
+fix_boot_df = fix_boot_df.T
+fix_boot_df.to_csv('results/msmall_fp_boot-vals_2026-05-25.csv')
+
+assert 1 == 0 
+
+# ### Thresholded Analyses
+
+# +
+proc_type = 'MSMAll_FIX'
+with open(glob(op.join(results_path, proc_type,  '*12-10**corr-uoiThresh*pkl'))[0], 'rb') as l:
+    thresh_fix_uoi_dict = pickle.load(l)
+
+_ = [thresh_fix_uoi_dict[sub].pop("ses-full") for sub in thresh_fix_uoi_dict.keys()] 
+
+with open(glob(op.join(results_path, proc_type, '*12-10**corr-lassoThresh*pkl'))[0], 'rb') as l:
+    thresh_msmall_lasso_dict = pickle.load(l)
+
+_ = [thresh_msmall_lasso_dict[sub].pop("ses-full") for sub in thresh_msmall_lasso_dict.keys()] 
+
+
+# +
+corr_dict, fp_list, _ = fingerprint(thresh_msmall_lasso_dict, thresh_msmall_lasso_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(thresh_msmall_lasso_dict)) 
+    
+acc_list.sort()
+np.mean(acc_list) 
+
+msmall_dict['thresh_lasso']['avg'] = np.mean(acc_list)
+msmall_dict['thresh_lasso']['lower'] =  acc_list[50]
+msmall_dict['thresh_lasso']['upper'] =  acc_list[-50]
+msmall_dict['thresh_lasso']['boot_vals'] = acc_list
+
+# +
+corr_dict, fp_list, _ = fingerprint(thresh_msmall_uoi_dict, thresh_msmall_uoi_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(thresh_msmall_uoi_dict)) 
+    
+acc_list.sort()
+np.mean(acc_list) 
+
+msmall_dict['thresh_uoi']['avg'] = np.mean(acc_list)
+msmall_dict['thresh_uoi']['lower'] =  acc_list[50]
+msmall_dict['thresh_uoi']['upper'] =  acc_list[-50]
+msmall_dict['thresh_uoi']['boot_vals'] = acc_list
+
+# +
+proc_type = 'xcpd'
+with open(glob(op.join(results_path, proc_type,  '*12-10**corr-uoiThresh*pkl'))[0], 'rb') as l:
+    thresh_xcpd_uoi_dict = pickle.load(l)
+
+_ = [thresh_xcpd_uoi_dict[sub].pop("ses-full") for sub in thresh_xcpd_uoi_dict.keys()] 
+
+with open(glob(op.join(results_path, proc_type, '*12-10**corr-lassoThresh*pkl'))[0], 'rb') as l:
+    thresh_xcpd_lasso_dict = pickle.load(l)
+
+_ = [thresh_xcpd_lasso_dict[sub].pop("ses-full") for sub in thresh_xcpd_lasso_dict.keys()] 
+
+
+# +
+corr_dict, fp_list, _ = fingerprint(thresh_xcpd_uoi_dict, thresh_xcpd_uoi_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(thresh_xcpd_uoi_dict)) 
+    
+acc_list.sort()
+np.mean(acc_list) 
+
+
+xcpd_dict['thresh_uoi']['avg'] = np.mean(acc_list)
+xcpd_dict['thresh_uoi']['lower'] =  acc_list[50]
+xcpd_dict['thresh_uoi']['upper'] =  acc_list[-50]
+xcpd_dict['thresh_uoi']['boot_vals'] = acc_list
+
+# +
+corr_dict, fp_list, _ = fingerprint(thresh_xcpd_lasso_dict, thresh_xcpd_lasso_dict)
+
+acc_list = [] 
+
+for i in range(1000): 
+    acc_list.append(sum(random.choices(fp_list, k=len(fp_list))) / len(thresh_xcpd_lasso_dict)) 
+    
+acc_list.sort()
+np.mean(acc_list) 
+
+
+xcpd_dict['thresh_lasso']['avg'] = np.mean(acc_list)
+xcpd_dict['thresh_lasso']['lower'] =  acc_list[50]
+xcpd_dict['thresh_lasso']['upper'] =  acc_list[-50]
+xcpd_dict['thresh_lasso']['boot_vals'] = acc_list
+# -
+
 xcpd_boot_df = pd.DataFrame.from_dict([xcpd_dict['lasso']['boot_vals'], 
-                                 xcpd_dict['pearson']['boot_vals'],
-                                  xcpd_dict['uoi']['boot_vals'] ])
-xcpd_boot_df.index = ['lasso', 'pearson', 'uoi']
+                                       xcpd_dict['pearson']['boot_vals'],
+                                       xcpd_dict['uoi']['boot_vals'], 
+                                       xcpd_dict['thresh_lasso']['boot_vals'] , 
+                                       xcpd_dict['thresh_uoi']['boot_vals']])
+xcpd_boot_df.index = ['lasso', 'pearson', 'uoi', 'thresh_lasso', 'thresh_uoi']
 xcpd_boot_df = xcpd_boot_df.T
-xcpd_boot_df.to_csv('xcpd_boot-vals_2025-12-10.csv')
+xcpd_boot_df.to_csv('xcpd_fp_boot-vals_2025-12-10.csv')
 
 msmall_boot_df = pd.DataFrame.from_dict([msmall_dict['lasso']['boot_vals'], 
-                                 msmall_dict['pearson']['boot_vals'],
-                                  msmall_dict['uoi']['boot_vals'] ])
-msmall_boot_df.index = ['lasso', 'pearson', 'uoi']
+                                         msmall_dict['pearson']['boot_vals'],
+                                         msmall_dict['uoi']['boot_vals'] , 
+                                         msmall_dict['thresh_lasso']['boot_vals'] , 
+                                         msmall_dict['thresh_uoi']['boot_vals']])
+msmall_boot_df.index = ['lasso', 'pearson', 'uoi', 'thresh_lasso', 'thresh_uoi']
 msmall_boot_df = msmall_boot_df.T
-msmall_boot_df.to_csv('msmall_boot-vals_2025-12-10.csv')
+msmall_boot_df.to_csv('results/msmall_fp_boot-vals_2026_04_27.csv')
+
+assert 0 == 1
 
 for ii in xcpd_dict.keys(): 
     del xcpd_dict[ii]['boot_vals']
@@ -253,26 +469,89 @@ pd.DataFrame.from_dict(xcpd_dict).to_csv(op.join(results_path,
 pd.DataFrame.from_dict(msmall_dict).to_csv(op.join(results_path,
     'msmall_ci_2025_12-10.csv'))
 
-msmall_boot_df['model'] = 'msmall'
+msmall_boot_df['proc_type'] = 'msmall'
 
-xcpd_boot_df['model'] = 'xcpd'
+xcpd_boot_df['proc_type'] = 'xcpd'
 
 plot_df = pd.concat([msmall_boot_df, xcpd_boot_df]) 
+
 
 msmall_df = pd.DataFrame.from_dict(msmall_dict) 
 xcpd_df = pd.DataFrame.from_dict(xcpd_dict) 
 
+plot_df = plot_df.melt(id_vars=['proc_type'])
+
+
+plot_df = plot_df.replace({'msmall': 'MinProc',
+                'xcpd': 'XCPD',
+                'lasso': 'LASSO',
+                'uoi': 'UoI', 
+                 'pearson': 'Pearson'})
+
+plot_df = plot_df.rename(columns={'variable':'model'}) 
+
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-palette_dict= {'Pearson': (0.12156862745098039, 0.4666666666666667, 0.7058823529411765),
- 'UoI': (1.0, 0.4980392156862745, 0.054901960784313725),
- 'LASSO': (0.17254901960784313, 0.6274509803921569, 0.17254901960784313)}
 
 colors = sns.color_palette()
 
 
-colors
+models = ['Pearson',  'LASSO', 'UoI', ]
+pipelines = ['MinProc', 'XCPD']
+labels = models + pipelines
+palette_colors = sns.color_palette("deep")
+palette_dict = {labels: color for labels, color in zip(labels, palette_colors)}
+
+
+palette_dict
+
+# +
+# plt.figure(figsize=(5, 5))
+
+g = sns.violinplot(data=plot_df, 
+            x='proc_type', 
+            y='value',
+           hue='model', 
+               order=pipelines,
+               hue_order=models,
+              palette=[palette_dict.get(key) for key in models], 
+             legend=False
+) 
+g.set_xlabel('') 
+# g.legend(loc="upper right", ncol=len(models))
+g.set_ylabel('Accuracy', weight='semibold')
+plt.title('Fingerprinting Identification', weight='bold')
+##for consistency
+plt.grid(axis='y', linewidth=.25)
+sns.despine()
+plt.tight_layout() 
+
+plt.savefig('plots/final_fingerprinting.png', 
+            dpi=400,
+            bbox_inches='tight',)
+
+# +
+# plt.figure(figsize=(6,4))
+
+g = sns.violinplot(data=plot_df, 
+            x='model', 
+            y='value',
+           hue='proc_type', 
+               order=models,
+               hue_order=pipelines,
+              palette=[palette_dict.get(key) for key in pipelines], 
+                   legend=False
+) 
+g.set_ylabel('Accuracy', weight='semibold')
+plt.title('Fingerprinting Identification', weight='bold')
+g.set_xlabel('') 
+
+##for consistency
+plt.grid(axis='y', linewidth=.25)
+sns.despine()
+plt.savefig('plots/final_fingerprinting_2.png', 
+            dpi=400,
+            bbox_inches='tight',)
 
 # +
 sns.set_palette('deep') 
@@ -307,6 +586,50 @@ plt.xlabel('Model', weight='semibold')
 plt.savefig('plots/2025-12-10_fingerprinting_acc.png', 
             transparent=True) 
 # -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
